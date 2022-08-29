28c88968d8774b375fce1b18bd66be149856cb97
lot8229 <lot8229@kakao.com>
2022-08-29 09:10:17 +0900

    sync test TFC

---
+++ version.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "ap-northeast-2"
+  profile = "default"  
+}
+
+terraform {
+  backend "remote" {
+    organization = "codns"
+    workspaces {
+      name = "TerraForm_tutorial"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
