64f6e1b50a5f54327719bf0c7483ce419d621d28
lot8229 <lot8229@kakao.com>
2022-08-30 19:38:19 +0900

    security_group ecs_task

--- secruity-group.tf
+++ secruity-group.tf
@@ -22,3 +22,19 @@ resource "aws_security_group" "lb" {
     to_port = 0
   } 
 }
+
+resource "aws_security_group" "ecs_task" {
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.lb.id]
+  }
+  egress {
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
